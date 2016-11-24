d4b512b2d97dde3b2e92c8542de3a625676ed523
Rohit Colaco <rohit.colaco@act-on.net>
2016-11-23 23:01:24 -0800

    first commit

---
+++ public/angular/default.js
@@ -0,0 +1,55 @@
+var __$aoc = angular.module("ngAppTest", []);
+
+__$aoc
+    .controller(
+        /* _NAME_         */ "BodyController",
+        /* _DEPENDENCIES_ */ [ "$scope", "$http",
+
+        function($scope, $http)
+        {
+            $scope.name = "Angular";
+
+            $http
+                .get("/gws/test/json")
+                .then(
+                    function(o){
+                        $scope.name = o.data.errorCode;
+                    },
+                    function(e){
+                        $scope.name = "ERROR: " + e.data;
+                    }
+                );
+        }
+    ])
+;
+
+__$aoc
+    .directive('focusOn', function(){
+    return function(scope, elem, attr){
+        scope.$on(attr.focusOn, function(e){
+            elem[0].focus();
+            try {
+                elem[0].select();
+            } catch(e) { }
+        });
+    };
+});
+
+__$aoc
+    .filter('orderObjectBy', function(){
+    return function(input, attribute) {
+        if (!angular.isObject(input)) return input;
+
+        var array = [];
+        for(var objectKey in input) {
+            input[objectKey].key = objectKey;
+            array.push(input[objectKey]);
+        }
+
+        array.sort(function(a, b){
+            var x = a[attribute], y = b[attribute];
+            return x > y ? 1 : x < y ? -1 : 0;
+        });
+        return array;
+    }
+});
